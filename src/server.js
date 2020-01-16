62866cec3bf242458f7865e7d0f332b6556d5fa0
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-16 21:39:45 +0900

    Creating GraphQL Server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('hello! server!');
+import dotenv from 'dotenv';
+import { GraphQLServer } from 'graphql-yoga';
+import logger from 'morgan';
+import schema from './schema';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+const PORT = process.env.ENV_PORT;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger('dev'));
+
+server.start({ port: PORT }, () => console.log(`Server running on: http://localhost:${PORT}`));
