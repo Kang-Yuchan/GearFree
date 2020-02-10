5db0869bd5d9815b84d8a4d55cfce1a169d63e74
Kang.Yuchan <ikimaku0517@gmail.com>
2020-02-10 23:25:24 +0900

    Delete Item

--- src/api/Item/editItem/editItem.js
+++ src/api/Item/editItem/editItem.js
@@ -1,24 +1,31 @@
 import { isAuthenticated } from '../../../middlewares';
 import { prisma } from '../../../../generated/prisma-client';
 
+const DELETE = 'DELETE';
+const EDIT = 'EDIT';
+
 export default {
 	Mutation: {
 		editItem: async (_, args, req) => {
 			isAuthenticated(req);
-			const { id, itemname, price, kind } = args;
+			const { id, itemname, price, kind, action } = args;
 			const { user } = req;
 			const item = await prisma.$exists.item({ id, user: { id: user.id } });
 			if (item) {
-				return prisma.updateItem({
-					data: {
-						itemname,
-						price,
-						kind
-					},
-					where: {
-						id
-					}
-				});
+				if (action === EDIT) {
+					return prisma.updateItem({
+						data: {
+							itemname,
+							price,
+							kind
+						},
+						where: {
+							id
+						}
+					});
+				} else if (action === DELETE) {
+					return prisma.deleteItem({ id });
+				}
 			} else {
 				throw Error("You can't do that");
 			}
